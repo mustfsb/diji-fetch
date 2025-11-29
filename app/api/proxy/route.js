6a5664fa60b3815b7bae07b58a6078376fa68149
Mustafa Said Bektaş <145277969+mustfsb@users.noreply.github.com>
2025-11-30 00:22:50 +0300

    Update route.js

--- app/api/proxy/route.js
+++ app/api/proxy/route.js
@@ -26,7 +26,7 @@ export async function GET(request) {
             headers: {
                 'Accept': 'application/json, text/plain, */*',
                 'Accept-Language': 'en-US,en;q=0.9',
-                'Cookie': '_ga=GA1.1.566625878.1762887069; ASP.NET_SessionId=mdkurduboxc3qoun20un0my5; cf_clearance=t8Rw.1IOCWC7noQkzPCXFtwF1hQClrKoDUl9rMCuBV8-1764410923-1.2.1.1-SE87tgzESIGYwWg0K6xbL8tkMdMZBEz_pUI2Ql6Gyd3fRfYTaxBiPjy3YTIoMOZycUg4HUS0KO2xPB9RqXfTcpyNdp4MZiQrf3R7qRBu8S06YVSWTi5CWQSc4gGQ30V8zvSfb98zAkLdGULfidehFDTFzxqjz7KYEvVUnn8CDP7b9m52yaKYSIbd7HtVhQCFmfWekR.8af2b7AP83Do5xhrV0wK8lVrPvIwdMg5zkrs; usrtkn=tkn=mdkurduboxc3qoun20un0my5; rememberMe=UserName=G36EW453V40VC2YFF3JRR3CSJ3AUM3S0S41AI39D&Password=G36EW4D0V464C4A4F3HM; _ga_L3RCSWSLG5=GS2.1.s1764408612$o7$g1$t1764411230$j59$l0$h0',
+                "Cookie": "_ga=GA1.1.566625878.1762887069; ASP.NET_SessionId=wyedmcqju4wjxqyyz3lk2sn0; cf_clearance=F3gxwUqUhtl.TP.lWWmfGHCc2RC2UZ0IjLOLeWtcDEM-1764450862-1.2.1.1-wYlgauCxdQJ7TLKLUyaDCPuhrFKOpnVliNLdZKjqveAb3srrQBMK96gQlpff.OPH_LY0tNhZDnM2EIK9JqmGiqxlhb.nQP7B23CBRQg3ClBFpaD0Qs9wIsXHzxQT9ItB2wqrxTGiCrQA6B_gZhpwHtyMQa1V2vnDv4quKu.v_ujAT3C26hJCEXS0HJ_nAdHiDYqOJJzFZ12eyB4sk8D8ewIARVbnGXh.xYSAUbH2S50; usrtkn=tkn=wyedmcqju4wjxqyyz3lk2sn0; rememberMe=UserName=C30GH3EFE37LE31CC3ENI30IO3IAB390F3DEO3IA&Password=C30GH3L0E3BSE4ECC3CM; _ga_L3RCSWSLG5=GS2.1.s1764450862$o9$g1$t1764450871$j51$l0$h0",
                 'User-Agent': 'DijidemiMobile/39 CFNetwork/3860.300.31 Darwin/25.2.0',
                 'Accept-Encoding': 'gzip, deflate, br',
                 'Connection': 'keep-alive',
